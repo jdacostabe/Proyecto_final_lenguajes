7ed49dd67f4a42d07283c71523c4abf27d50f5fb
jdacostabe <jdacostabe@unal.edu.co>
2020-12-13 00:25:15 -0500

    bugFix

---
+++ src/smell_useless_parameters/SmellUselessParameters.java
@@ -0,0 +1,9 @@
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class SmellUselessParameters implements Smeller {
+
+    @Override public String fix(ParseTree tree) {
+        
+        return null;
+    }
+}
